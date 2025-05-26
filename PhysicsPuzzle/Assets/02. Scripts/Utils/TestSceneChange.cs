8e73bb44ab4cabf6de4cf9d6b13774faa5e33c7f
Hyobin <37098177+ahb0923@users.noreply.github.com>
2025-05-26 15:12:24 +0900

    Feat: 비동기 씬전환 & 제네릭싱글톤
    매니저 오브젝트(파괴불가오브젝트 일괄관리)
    Singleton 제네릭 스크립트
    TestButton 이용하여 씬이동 체크
    TestSceneChage.cs에서 사용방법 확인가능

---
+++ PhysicsPuzzle/Assets/02. Scripts/Utils/TestSceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestSceneChange : MonoBehaviour
+{
+    [SerializeField] Button button;
+
+    private void Start()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    public void OnClick()
+    {
+        // 매개변수로 씬타입 값으로 지정
+        SceneHandleManager.Instance.LoadScene(SCENE_TYPE.TestGoalScene);
+    }
+}
