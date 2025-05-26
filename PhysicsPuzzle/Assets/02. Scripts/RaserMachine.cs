895c02283481d3abab8972fb12b2e9c9efcfbe21
Hyobin <37098177+ahb0923@users.noreply.github.com>
2025-05-26 19:51:13 +0900

    Chore: 레이저머신 기본 디자인

---
+++ PhysicsPuzzle/Assets/02. Scripts/RaserMachine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaserMachine : MonoBehaviour
+{
+    [SerializeField] Transform _pillarAngle;
+    [SerializeField] Transform _colorBottle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
