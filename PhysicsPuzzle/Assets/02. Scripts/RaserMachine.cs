ac77293255caed13c011412d701805eb52cedead
Hyobin <37098177+ahb0923@users.noreply.github.com>
2025-05-26 20:03:00 +0900

    Chore: 레이저 머신 레이저 추가
    space로 테스트 발사 가능

--- PhysicsPuzzle/Assets/02. Scripts/RaserMachine.cs
+++ PhysicsPuzzle/Assets/02. Scripts/RaserMachine.cs
@@ -7,15 +7,29 @@ public class RaserMachine : MonoBehaviour
     [SerializeField] Transform _pillarAngle;
     [SerializeField] Transform _colorBottle;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public LineRenderer lineRenderer;
+    public float maxDistance = 100f;
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space)) // Space 누르면 발사
+        {
+            Ray ray = new Ray(_pillarAngle.position, transform.up);
+            RaycastHit hit;
+
+            Vector3 endPosition = _pillarAngle.position + transform.up * maxDistance;
+
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                endPosition = hit.point;
+            }
+
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, endPosition);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position);
+        }
     }
 }
