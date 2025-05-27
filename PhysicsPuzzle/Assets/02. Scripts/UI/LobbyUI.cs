d35989750a8585dcb66ae7421e01ca96e980e2f2
leejy1685 <ucc1685@naver.com>
2025-05-27 18:49:04 +0900

    fix : 로비카메라에 로비 UI 생성
    로비카메라에 로비 UI 생성

--- PhysicsPuzzle/Assets/02. Scripts/UI/LobbyUI.cs
+++ PhysicsPuzzle/Assets/02. Scripts/UI/LobbyUI.cs
@@ -1,5 +1,7 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LobbyUI : BaseUI
@@ -23,14 +25,7 @@ public class LobbyUI : BaseUI
     public override void Init(UIManager uiManager)
     {
         base.Init(uiManager);
-    }
-    
-    // 인스펙터에서 직접 연결. 씬에 슬라이더 추가했다고 가정, 슬라이더 설정은 되어 있을거고. 
-    // onvalueChangedEvent로. dropDown을 펼쳐서 +버튼을 하신 다음에. 사운드매니저를, 할당해서 우측 함수 목록해서
-    // 마스터슬라이드, 셋마스터 넣고, bgm은 bgm넣고. 
-    
-    private void Start()
-    {
+        
         startButton.onClick.AddListener(OnClickStartButton);
         loadButton.onClick.AddListener(OnClickLoadButton);
         optionButton.onClick.AddListener(OnClickOptionButton);
@@ -38,10 +33,17 @@ public class LobbyUI : BaseUI
         
         bgmSlider.onValueChanged.AddListener((value) => SoundManager.Instance.SetBGMVolume(value));;
         sfxSlider.onValueChanged.AddListener((value) => SoundManager.Instance.SetSFXVolume(value));;
-
-        SoundManager.Instance.Play("GravityScene");
     }
     
+    // 인스펙터에서 직접 연결. 씬에 슬라이더 추가했다고 가정, 슬라이더 설정은 되어 있을거고. 
+    // onvalueChangedEvent로. dropDown을 펼쳐서 +버튼을 하신 다음에. 사운드매니저를, 할당해서 우측 함수 목록해서
+    // 마스터슬라이드, 셋마스터 넣고, bgm은 bgm넣고. 
+
+    private void Start()
+    {
+        SoundManager.Instance.Play(SceneManager.GetActiveScene().name);
+    }
+
     private void OnClickStartButton()
     {
         UIManager.Instance.OnClickStart();
