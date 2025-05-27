94c0ab9798f72ab067467d6701461a6cf4c1e568
leejy1685 <ucc1685@naver.com>
2025-05-27 09:47:32 +0900

    feat : 낮과 밤 변경 기능 추가
    낮과 밤 변경 기능 추가

--- PhysicsPuzzle/Assets/02. Scripts/DayAndNight/DayAndNight.cs
+++ PhysicsPuzzle/Assets/02. Scripts/DayAndNight/DayAndNight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,62 +7,115 @@ public class DayAndNight : MonoBehaviour
 {
     // 하루 시간을 0 ~ 1로 치환
     // 길이를 늘리고 싶으면 fullDayLength를 조절
-    [Range(0.0f, 1.0f)]
-    public float time;
+    [Range(0.0f, 1.0f)] public float time;
     public float fullDayLength;
     public float startTime = 0.4f;
     private float timeRate;
-    public Vector3 noon;
+    [SerializeField] private Vector3 noon;
 
     [Header("Sun")]
     public Light sun;
-    public Gradient sunColor;
-    public AnimationCurve sunIntensity;
+    [SerializeField] private Gradient sunColor;
+    [SerializeField] private AnimationCurve sunIntensity;
+    private Coroutine ChangeDayCoroutine;
 
     [Header("Moon")]
     public Light moon1;
     public Light moon2;
-    public Gradient moonColor;
-    public AnimationCurve moonIntensity;
+    [SerializeField] private Gradient moonColor;
+    [SerializeField] private AnimationCurve moonIntensity;
+    private Coroutine ChangeNightCoroutine;
 
     [Header("Other Lighting")]
-    public AnimationCurve lightingIntensityMultiplier;
-    public AnimationCurve reflectionIntensityMultiplier;
+    [SerializeField] private AnimationCurve lightingIntensityMultiplier;
+    [SerializeField] private AnimationCurve reflectionIntensityMultiplier;
 
     private void Start()
     {
         timeRate = 1.0f / fullDayLength;
         time = startTime;
     }
-
+    
     private void Update()
     {
-        time = (time + timeRate * Time.deltaTime) % 1.0f;
-
-        UpdateLighting(sun, sunColor, sunIntensity);
-        UpdateLighting(moon1, moonColor, moonIntensity);
-        UpdateLighting(moon2, moonColor, moonIntensity);
-
-        // Evaluate는 Inspector에 그린 그래프에서 time을 입력받으면 특정 값을 return
-        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
-        RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
-
+        //test code
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangeDayAndNight();
+        }
     }
 
-    void UpdateLighting(Light lightSource, Gradient colorGradiant, AnimationCurve intensityCurve)
+    private void ChangeDayAndNight()
     {
-        float intensity = intensityCurve.Evaluate(time);
+        if (Mathf.Abs(time-0.5f) > 0.01f)
+        {
+            ChangeDayCoroutine = StartCoroutine(ChangeDay_Coroutine());
+        }
+        else
+        {
+            ChangeNightCoroutine = StartCoroutine(ChangeNight_Coroutine());
+        }
+    }
+    
 
-        // 하루 시간(0 ~ 1)과 해/달의 자전주기(0 ~ 360)의 값을 동기화.
-        // 해와 달은 180도 차이가 항상 나기 때문에 0.5f(180/360)의 차이
-        lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4.0f;
-        lightSource.color = colorGradiant.Evaluate(time);
-        lightSource.intensity = intensity;
+    IEnumerator ChangeNight_Coroutine()
+    {
+        while (true)
+        {
+            //시간 계산
+            time = (time + timeRate * Time.deltaTime) % 1.0f;
+            
+            //태양 위치 변경
+            sun.transform.eulerAngles =  (time - 0.25f) * 4.0f * noon;
+            sun.color = sunColor.Evaluate(time);
+            sun.intensity = sunIntensity.Evaluate(time);
+            
+            //달 밝기 변경
+            moon1.color = moonColor.Evaluate(time);
+            moon1.intensity = moonIntensity.Evaluate(time);
+            
+            moon2.color = moonColor.Evaluate(time);
+            moon2.intensity = moonIntensity.Evaluate(time);
+            
+            RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+            RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
 
-        GameObject go = lightSource.gameObject;
-        if (lightSource.intensity == 0 && go.activeInHierarchy)
-            go.SetActive(false);
-        else if (lightSource.intensity > 0 && !go.activeInHierarchy)
-            go.SetActive(true);
+            //밤이 되면 탈출
+            if (Mathf.Abs(time) < 0.01f)
+                break;
+            
+
+            yield return null;
+        }
+    }
+    
+    IEnumerator ChangeDay_Coroutine()
+    {
+        while (true)
+        {
+            //시간 계산
+            time = (time + timeRate * Time.deltaTime) % 1.0f;
+            
+            //태양 위치 변경
+            sun.transform.eulerAngles =  (time - 0.25f) * 4.0f * noon;
+            sun.color = sunColor.Evaluate(time);
+            sun.intensity = sunIntensity.Evaluate(time);
+            
+            //달 밝기 변경
+            moon1.color = moonColor.Evaluate(time);
+            moon1.intensity = moonIntensity.Evaluate(time);
+            
+            moon2.color = moonColor.Evaluate(time);
+            moon2.intensity = moonIntensity.Evaluate(time);
+            
+            RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+            RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+
+            //낮이 되면 탈출
+            if (Mathf.Abs(time-0.5f) < 0.01f)
+                break;
+            
+            yield return null;
+        }
     }
 }
