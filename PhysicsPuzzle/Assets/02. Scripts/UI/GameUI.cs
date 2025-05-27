83a05489f4727462eb73bb5c275b5a24a1205062
hbks0224 <hbks1234@gmail.com>
2025-05-27 17:43:57 +0900

    feat : UI 해상도 수정

--- PhysicsPuzzle/Assets/02. Scripts/UI/GameUI.cs
+++ PhysicsPuzzle/Assets/02. Scripts/UI/GameUI.cs
@@ -1,9 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.XR.GoogleVr;
 using UnityEngine;
 
 public class GameUI : BaseUI
 {
+   
+    [Header("Top UI")]
+    [SerializeField] private GameObject _nowTImeIcon;
+    [SerializeField] private TextMeshProUGUI _stageNameText;
+    [SerializeField] private TextMeshProUGUI _timeText;
+
+    [Header("Bottom UI")]
+    [SerializeField] private GameObject _changeGravityUI;
+    [SerializeField] private GameObject _changeTimeUI;
+    [SerializeField] public GameObject InventoryBG;
+
+
     protected override UIState GetUIState()
     {
         return UIState.Game;
@@ -13,4 +27,8 @@ public class GameUI : BaseUI
     {
         base.Init(uiManager);
     }
+
+
+
+
 }
