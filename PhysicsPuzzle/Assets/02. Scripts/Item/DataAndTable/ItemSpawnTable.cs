190bbcac8505273d5ca525c3f4a99019b4e4b4af
Bangeunseong <bangeunseong@naver.com>
2025-05-30 21:04:48 +0900

    [feat] Added Item Spawn System in stage 1

---
+++ PhysicsPuzzle/Assets/02. Scripts/Item/DataAndTable/ItemSpawnTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace _02._Scripts.Item.DataAndTable
+{
+    [CreateAssetMenu(fileName = "ItemSpawnTable", menuName = "Item/ItemSpawnTable", order = 0)]
+    public class ItemSpawnTable : ScriptableObject
+    {
+        [SerializeField] private List<ItemSpawnData> itemSpawnData = new();
+        [SerializeField] private SerializedDictionary<int, ItemSpawnData> itemSpawnDataDictionary = new();
+
+        private void OnEnable()
+        {
+            var i = 0;
+            foreach (var data in itemSpawnData) itemSpawnDataDictionary.TryAdd(i++, data);
+        }
+        
+        public ItemSpawnData GetItemSpawnData(int index)
+        {
+            return !itemSpawnDataDictionary.TryGetValue(index, out var data) ? null : data;
+        }
+    }
+}
---
+++ PhysicsPuzzle/Assets/02. Scripts/Managers/Item/ItemSpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using _02._Scripts.Item;
+using _02._Scripts.Item.DataAndTable;
+using _02._Scripts.Utils;
+using UnityEngine;
+
+namespace _02._Scripts.Managers.Item
+{
+    public class ItemSpawnManager : MonoBehaviour
+    {
+        [Header("ItemBox Spawn Settings")]
+        [SerializeField] private List<Transform> itemBoxSpawnPoints;
+        [SerializeField] private GameObject itemBoxPrefab;
+        
+        [Header("Item Spawn Data Settings")]
+        [SerializeField] private ItemSpawnTable itemSpawnTable;
+        
+        // Singleton
+        public static ItemSpawnManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+            } else { if (Instance != this) Destroy(gameObject); }
+        }
+        
+        public void SpawnItemBoxes()
+        {
+            var i = 0;
+            foreach (var itemBoxSpawnPoint in itemBoxSpawnPoints)
+            {
+                var go = Instantiate(itemBoxPrefab, itemBoxSpawnPoint.position, Quaternion.identity);
+                var itemBox = Helper.GetComponent_Helper<ItemBox>(go);
+                itemBox.Init(i, itemSpawnTable.GetItemSpawnData(i++));
+                itemBox.SpawnItems();
+            }
+        }
+    }
+}
